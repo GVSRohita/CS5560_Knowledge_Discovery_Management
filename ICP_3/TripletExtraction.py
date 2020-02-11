2d2e9f27a4a4906203a08f41d88bc799a0c57e8c
Poonam Kankariya <42658812+pkankariya@users.noreply.github.com>
2020-02-10 18:37:48 -0600

    Update output display

--- ICP_3/TripletExtraction.py
+++ ICP_3/TripletExtraction.py
@@ -3,22 +3,23 @@ import spacy
 import textacy
 
 # Read input data in the form of text sentence(s)
-text = "Barrack Obama was born in Hawaii"
-# text = "'CHICAGO (AP) —Citing high fuel prices, United Airlines said Friday it has increased fares by $6 per round trip on flights to some cities also served by lower-cost carriers. American Airlines, a unit AMR, immediately matched the move, spokesman Tim Wagner said. United, a unit of UAL, said the increase took effect Thursday night and applies to most routes where it competes against discount carriers, such as Chicago to Dallas and Atlanta and Denver to San Francisco, Los Angeles and New York.'"
+# text = "Barrack Obama was born in Hawaii"
+text = "'CHICAGO (AP) —Citing high fuel prices, United Airlines said Friday it has increased fares by $6 per round trip on flights to some cities also served by lower-cost carriers. American Airlines, a unit AMR, immediately matched the move, spokesman Tim Wagner said. United, a unit of UAL, said the increase took effect Thursday night and applies to most routes where it competes against discount carriers, such as Chicago to Dallas and Atlanta and Denver to San Francisco, Los Angeles and New York.'"
 
-# Triplet extraction process # 1
-nlp = spacy.load('en')
-for sentence in text.split("."):
-    val = nlp(sentence)
-    tuples = textacy.extract.subject_verb_object_triples(val)
-    tuples_list = []
-    if tuples:
-        tuples_to_list = list(tuples)
-        tuples_list.append(tuples_to_list)
-        print(tuples_list)
+# # Triplet extraction process # 1
+# nlp = spacy.load('en')
+# for sentence in text.split("."):
+#     val = nlp(sentence)
+#     tuples = textacy.extract.subject_verb_object_triples(val)
+#     tuples_list = []
+#     if tuples:
+#         tuples_to_list = list(tuples)
+#         tuples_list.append(tuples_to_list)
+#         print(tuples_list)
 
 # Triplet extraction process # 2
 nlp = spacy.load('en')
 textExtract = nlp(text)
 text_ext = textacy.extract.subject_verb_object_triples(textExtract)
-print('Triplets extracted from the input provided are:', text_ext)
+# print('Triplets extracted from the input provided are:', type(text_ext))
+print('Triplets extracted from the input provided are:', *text_ext, sep='\n')
